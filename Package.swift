90441137b4c52bf44ddc9210fe3a86e2d61e128f
Stanislav Redreiev <moderator.promo@gmail.com>
2021-12-14 22:44:20 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -25,6 +25,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "StyleOrganizer",
-            path: "StyleOrganizer/Classes")
+            path: "StyleOrganizer/Classes/")
     ]
 )
